a096dc0a60b6c49420ddc0c91b6713edeac8d7f7
codekorn <codekorn@gmail.com>
2022-06-15 06:42:31 -0400

    Checking Response

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -63,7 +63,7 @@ class EditProfile extends Component {
             }
             catch(error)
             {
-                $("#errorText").text(response);
+                $("#errorText").text(response['status']);
                 $("#errorText").show();
             }
         }
